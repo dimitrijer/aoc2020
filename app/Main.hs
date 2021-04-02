521e16428ecdaf116b45aa588ae11a8eb7340cd4
Dimitrije Radojević <dimitrijer@nordeus.com>
2021-04-02 19:13:48 +0200

    Clean up main a bit

--- app/Main.hs
+++ app/Main.hs
@@ -6,42 +6,28 @@ import Day03
 import Day04
 import Day05
 import Day06
+import Day07
 import System.Environment
+import Text.Printf
+
+exec :: String -> Int -> IO ()
+exec input day = do
+  case day of
+    1 -> do print $ Day01.partOne input; print $ Day01.partTwo input
+    2 -> do print $ Day02.partOne input; print $ Day02.partTwo input
+    3 -> do print $ Day03.partOne input; print $ Day03.partTwo input
+    4 -> do print $ Day04.partOne input; print $ Day04.partTwo input
+    5 -> do print $ Day05.partOne input; print $ Day05.partTwo input
+    6 -> do print $ Day06.partOne input; print $ Day06.partTwo input
+    7 -> do print $ Day07.partOne input; print $ Day07.partTwo input
+    _ -> do putStrLn $ "Unknown day: " ++ show day
 
 main :: IO ()
 main = do
   args <- getArgs
   if length args == 1
-    then case read $ head args :: Int of
-      1 ->
-        do
-          input <- readFile "resources/day01.txt"
-          print $ Day01.partOne input
-          print $ Day01.partTwo input
-      2 ->
-        do
-          input <- readFile "resources/day02.txt"
-          print $ Day02.partOne input
-          print $ Day02.partTwo input
-      3 ->
-        do
-          input <- readFile "resources/day03.txt"
-          print $ Day03.partOne input
-          print $ Day03.partTwo input
-      4 ->
-        do
-          input <- readFile "resources/day04.txt"
-          print $ Day04.partOne input
-          print $ Day04.partTwo input
-      5 ->
-        do
-          input <- readFile "resources/day05.txt"
-          print $ Day05.partOne input
-          print $ Day05.partTwo input
-      6 ->
-        do
-          input <- readFile "resources/day06.txt"
-          print $ Day06.partOne input
-          print $ Day06.partTwo input
-      _ -> putStrLn "Unknown day."
+    then do
+      let day = read $ head args
+      input <- readFile $ printf "resources/day%02d.txt" day
+      exec input day
     else putStrLn "Please input day number."
